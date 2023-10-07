424d76f5c189cf59751dab52c76b774de4d1b876
federico batalla <federicobatalla@tdf.edu.ar>
2023-10-07 11:57:52 -0300

    1er merge con fede

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-
+import Ingreso from '../Ingreso/Ingreso';
 import { NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <>
-            <p>header</p>
-            <nav>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/Institucional">Institucional</NavLink>
-                <NavLink to="/Documentaciono">Documentacion</NavLink>
-                <NavLink to="/Novedades">Novedades</NavLink>
-                <NavLink to="/Contacto">Contacto</NavLink>
-            </nav>
+            <header>
+                <div className='contenedor'>
+                    <Ingreso />
+                    <p>menu</p>
+                    <nav>
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="/Institucional">Institucional</NavLink>
+                        <NavLink to="/Documentaciono">Documentacion</NavLink>
+                        <NavLink to="/Novedades">Novedades</NavLink>
+                        <NavLink to="/Contacto">Contacto</NavLink>
+                    </nav>
+
+                </div>
+
+            </header>
+
 
         </>
     );
---
+++ src/Component/Ingreso/Ingreso.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Ingreso.css';
+
+const Ingreso = () => {
+    return (
+        <div className='contenedor'>
+            <div className='contenedor-header'>
+                <p> Gdor. Paz 165 , Ushuaia, Tierra del Fuego - ctpobarko.sec@tdf.edu.ar - 445660 </p>
+                <div className="btningreso">
+                    <button>Ingreso Alumnos</button>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Ingreso;
