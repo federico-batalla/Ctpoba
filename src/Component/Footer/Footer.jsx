9138dc6346114c288b108689054ed324b9f2db3e
federico batalla <federicobatalla@tdf.edu.ar>
2023-10-09 00:19:20 -0300

    cre creo el footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import './Footer.css'
+import Logo from './logobyn.png'
 
 const Footer = () => {
     return (
-        <div>
-            <p>footer</p>
-        </div>
+      <>
+        <footer>
+            <div className="contenedor">
+                <div className='graficos'>
+                    <div className='logo'>
+                        <img src={Logo} alt="" />
+                    </div>
+                    <div className="redes-sociales">
+                        
+                    </div>
+                </div>
+                <div className='datos'>
+
+                </div>
+                <div className='contacto'>
+
+                </div>
+            </div>
+        </footer>
+      </>
     );
 };
 
