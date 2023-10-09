b7c6b7b8a6e2181e3ceca2f19609e49bad99fac9
federico batalla <federicobatalla@tdf.edu.ar>
2023-10-09 12:11:06 -0300

    se completo el footer con los datos

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import './Footer.css'
 import Logo from './logobyn.png'
-import { AiFillFacebook, AiFillInstagram,AiFillYoutube } from "react-icons/ai";
+import { AiFillFacebook, AiFillInstagram, AiFillYoutube } from "react-icons/ai";
 import IconoRS from '../IconoRS/IconoRS';
 
 
 const Footer = () => {
     return (
-      <>
-        <footer>
-            <div className="contenedor">
-                <div className='graficos'>
-                    <div className='logo'>
-                        <img src={Logo} alt="" />
-                    </div>
-                    <div className="redes-sociales">
-                        <IconoRS  logo = <AiFillFacebook/> src='https://www.facebook.com/'/>
-                        <IconoRS  logo = <AiFillInstagram/> src = 'https://www.instagram.com/'/>
-                        <IconoRS  logo = <AiFillYoutube/> src = 'https://www.youtube.com/' />
-                    </div>
-                </div>
-                <div className='datos'>
+        <>
+            <footer>
+                <div className="contenedor ">
+                    <div className="cont-foot">
+                        <div className='graficos'>
+                            <div className='logo'>
+                                <img src={Logo} alt="" />
+                            </div>
+                            <div className="redes-sociales">
+                                <IconoRS logo=<AiFillFacebook /> src='https://www.facebook.com/' />
+                                <IconoRS logo=<AiFillInstagram /> src='https://www.instagram.com/' />
+                                <IconoRS logo=<AiFillYoutube /> src='https://www.youtube.com/' />
+                            </div>
+                        </div>
+                        <div className='datos'>
+                            <h2>Nuestro Colegio</h2>
+                            <p>Gdor. Paz 165 , Ushuaia, Tierra del Fuego</p>
+                            <p>Horario de atencion Presencial : de lunes a viernes de 8:00 Hs a 21:00 Hs</p>
+                        </div>
+                        <div className='contacto'>
 
+                        </div>
+                    </div>
                 </div>
-                <div className='contacto'>
-
-                </div>
-            </div>
-        </footer>
-      </>
+            </footer>
+        </>
     );
 };
 
