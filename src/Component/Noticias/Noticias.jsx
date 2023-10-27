0c6a65914c0549af3885a923188830e2e5a8747f
federico batalla <federicobatalla@tdf.edu.ar>
2023-10-27 12:04:50 -0300

    cambios

--- src/Component/Noticias/Noticias.jsx
+++ src/Component/Noticias/Noticias.jsx
@@ -5,26 +5,29 @@ import './Noticias.css'
 const Noticias = ({ articulos }) => {
 
     return (
-        
-            <div className='article-list'>
-                {articulos.map(articulo => (
-                    <div className="article-container" key={articulo.id}>
-                        <div className="img-container">
-                            <img src={articulo.image} alt={articulo.tittle} />
-                        </div>
-                        <div className="article-body">
-                            <h2>{articulo.tittle}</h2>
+
+        <div className='article-list'>
+            {articulos.map(articulo => (
+                <div className="article-container" key={articulo.id}>
+                    <div className="img-container">
+                        <img src={articulo.image} alt={articulo.tittle} />
+                    </div>
+                    <div className="article-body">
+                        <h2>{articulo.tittle}</h2>
+                        <div className="descripcion">
                             <p>{articulo.description}</p>
-                            <div className="article-footer">
-                                <span>{articulo.date}</span>
-                            </div>
+                        </div>
+
+                        <div className="article-footer">
+                            <span>{articulo.date}</span>
                         </div>
                     </div>
-                ))}
+                </div>
+            ))}
 
 
-            </div>
-        
+        </div>
+
     )
 };
 
